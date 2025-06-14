c65274a115dbf7244fb13012278c1c10ade72f65
atari-monk <atari.monk1@gmail.com>
2025-06-14 15:23:44 +0200

    fix logs issue

--- chrome_utils/chrome_automation.py
+++ chrome_utils/chrome_automation.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from chatgpt_utils.config.chatgpt_config import ChatGPTConfig
 from chrome_utils.chrome_profiles import get_chrome_profile
 from utils.valid_url import is_valid_url
+import os
 
 def open_chrome_with_profile(config: ChatGPTConfig) -> webdriver.Chrome | None:
     if not is_valid_url(config.page):
@@ -22,13 +24,24 @@ def open_chrome_with_profile(config: ChatGPTConfig) -> webdriver.Chrome | None:
     options = Options()
     options.add_argument(rf"user-data-dir={active_profile}") # type: ignore
     options.add_argument(f"profile-directory={profile_directory}") # type: ignore
-    options.add_experimental_option("excludeSwitches", ["enable-automation"]) # type: ignore
+    options.add_experimental_option("excludeSwitches", ["enable-automation", "enable-logging"]) # type: ignore
     options.add_argument("--disable-blink-features=AutomationControlled") # type: ignore
-
+    options.add_argument("--log-level=3") # type: ignore
+    options.add_argument("--disable-gpu") # type: ignore
+    options.add_argument("--disable-dev-shm-usage") # type: ignore
+    options.add_argument("--no-sandbox") # type: ignore
+    
     if config.detach:
         options.add_experimental_option("detach", True) # type: ignore
 
-    driver = webdriver.Chrome(options=options)
+    service = Service()
+    service.creationflags = 0x08000000  # type: ignore
+    service.quiet = True # type: ignore
+
+    os.environ['WDM_LOG_LEVEL'] = '0'
+    os.environ['WDM_PRINT_FIRST_LINE'] = 'False'
+
+    driver = webdriver.Chrome(service=service, options=options)
     driver.get(config.page)
     print(f"Successfully opened: {config.page}")
     return driver
